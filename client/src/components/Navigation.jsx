9b6e99beb95df9f69024da6b362b715a4815a5b4
Milen Ivanov <ivanovvmilen@gmail.com>
2020-02-19 15:25:43 +0200

    Added navigatin bar

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default function Navigation() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Navbar
+      </Link>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/current-rentals">
+              Dashboard <span className="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/cars">
+              Cars
+          </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+  )
+}
