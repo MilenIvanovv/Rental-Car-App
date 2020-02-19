fbb0174cba443f9c3469336053ece599250712d5
Milen Ivanov <ivanovvmilen@gmail.com>
2020-02-19 10:57:20 +0200

    Added car status to car entity

--- server/src/database/entities/cars.entity.ts
+++ server/src/database/entities/cars.entity.ts
@@ -1,4 +1,10 @@
 import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { CarClasses } from '../../common/car-classes.enum';
+import { CarStatus } from '../../common/car-status.enum';
+
+const imgPlaceholder = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQmKLUDYJ-YQug7Xu4rEEpRx_0xkxVEyaGQB4qhxFmxZW4koO7S';
+type classes = 'A' | 'B' | ' C' | 'D';
+type status = 'borrowed' | 'listed';
 
 @Entity('cars')
 export class Car extends BaseEntity {
@@ -7,4 +13,13 @@ export class Car extends BaseEntity {
 
   @Column({ type: 'varchar', length: 100, nullable: false })
   model: string;
+
+  @Column({ type: 'varchar', default: imgPlaceholder })
+  picture: string;
+
+  @Column({ type: 'enum', enum: ['A', 'B',' C', 'D']})
+  class: CarClasses;
+
+  @Column({ type: 'enum', enum: ['borrowed', 'listed']})
+  carStatus: CarStatus;
 }
