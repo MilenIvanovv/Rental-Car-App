64349d9d38dc82eeaca9a45e7f0a4e9e80053c03
Milen Ivanov <ivanovvmilen@gmail.com>
2020-02-20 18:09:23 +0200

    Added prop types to Card list component

--- client/src/components/CarsList.jsx
+++ client/src/components/CarsList.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import CarCard from './carCard/CarCard';
 
 export default function CarsList(props) {
-  const cars = props.cars.map((car) => <CarCard key={car.model} car={car} />);
+  const { cars } = props;
+  const carElements = cars.map((car) => <CarCard key={car.model} car={car} />);
   return (
     <div className="row">
-      {cars}
+      {carElements}
     </div>
   );
 }
+
+CarsList.propTypes = {
+  cars: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    model: PropTypes.string,
+    class: PropTypes.string,
+    price: PropTypes.string,
+    picture: PropTypes.string,
+  })).isRequired,
+};
