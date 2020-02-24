ae056ad8c73c9893a8f1b17921201e36964e1b10
Milen Ivanov <ivanovvmilen@gmail.com>
2020-02-24 14:14:33 +0200

    Get rentals request

--- client/src/components/CurrentRentalsPage.jsx
+++ client/src/components/CurrentRentalsPage.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 import RentedCarsTable from './rentedCarsTable/RentedCarsTable';
+import { API_ROOT } from '../constants/constants';
 
-export default function CurrentRentals() {
-  return (
-    <div>
-      <div className="p-3">
-        <h3>Current rentals</h3>
-        <RentedCarsTable />
+class CurrentRentals extends Component {
+  async componentDidMount() {
+    const rentals = await axios.get(`${API_ROOT}/rentals`);
+    console.log(rentals);
+  }
+
+  render() {
+    const { rentals } = this.props;
+
+    return (
+      <div>
+        <div className="p-3">
+          <h3>Current rentals</h3>
+          <RentedCarsTable rentals={[]} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+const mapStateToProps = (state) => ({
+  rentals: state.rentals,
+});
+
+export default connect(mapStateToProps)(CurrentRentals);
+
+// CurrentRentals.propTypes = {
+//   rentals: PropTypes.arrayOf(PropTypes.shape({
+//     id: PropTypes.number.isRequired,
+//     estimatedDate: PropTypes.string.isRequired,
+//     firstName: PropTypes.string.isRequired,
+//     lastName: PropTypes.string.isRequired,
+//     age: PropTypes.number.isRequired,
+//     status: PropTypes.oneOf(['open', 'closed']).isRequired,
+//     returnDate: PropTypes.string.isRequired,
+//     car: PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       model: PropTypes.string.isRequired,
+//       picture: PropTypes.string.isRequired,
+//     }).isRequired,
+//   })).isRequired,
+// };
--- client/src/components/rentedCarsTable/RentedCarsTable.jsx
+++ client/src/components/rentedCarsTable/RentedCarsTable.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import RentedCarsRow from './RentedCarsRow';
 import './RentedCarsTable.css';
 
-export default function RentedCarsTable() {
-  const rows = [
-    {},
-    {},
-    {},
-    {},
-    {},
-  ];
 
-  const elements = rows.map((rental) => <RentedCarsRow key={rental.model} rental={rental} />);
+export default function RentedCarsTable(props) {
+  const { rentals } = props;
+  const elements = rentals
+    .map((rental) => <RentedCarsRow key={rental.car.model} rental={rental} />);
 
   return (
     <table className="table">
@@ -33,3 +29,34 @@ export default function RentedCarsTable() {
     </table>
   );
 }
+
+// RentedCarsTable.propTypes = {
+//   rentals: PropTypes.arrayOf(PropTypes.shape({
+//     id: PropTypes.number.isRequired,
+//     estimatedDate: PropTypes.string.isRequired,
+//     firstName: PropTypes.string.isRequired,
+//     lastName: PropTypes.string.isRequired,
+//     age: PropTypes.number.isRequired,
+//     status: PropTypes.oneOf(['open', 'closed']).isRequired,
+//     returnDate: PropTypes.string.isRequired,
+//     car: PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       model: PropTypes.string.isRequired,
+//       picture: PropTypes.string.isRequired,
+//     }).isRequired,
+//   })).isRequired,
+// };
+
+RentedCarsTable.propTypes = {
+  rentals: PropTypes.arrayOf(PropTypes.shape({
+    model: PropTypes.string.isRequired,
+    customer: PropTypes.string.isRequired,
+    dateFrom: PropTypes.string.isRequired,
+    estimatedDate: PropTypes.string.isRequired,
+    estimatedDays: PropTypes.string.isRequired,
+    estimatedPricePerDay: PropTypes.string.isRequired,
+    curDaysRented: PropTypes.string.isRequired,
+    curPricePerDay: PropTypes.string.isRequired,
+    curTotalPrice: PropTypes.string.isRequired,
+  })),
+};
