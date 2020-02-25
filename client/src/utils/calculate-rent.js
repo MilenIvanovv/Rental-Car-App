f9363679cbf410c82869a0d0b6ec406e555afd6e
Milen Ivanov <ivanovvmilen@gmail.com>
2020-02-25 12:09:26 +0200

    Calculate days method implementation

--- client/src/utils/calculate-rent.js
+++ client/src/utils/calculate-rent.js
@@ -11,7 +11,20 @@
 
 
 export const days = (from, to) => {
+  if (!from instanceof Date  || from == 'Invalid Date') {
+    throw new Error('Invalid date');
+  }
 
+  if (!to instanceof Date || to == 'Invalid Date') {
+    throw new Error('Invalid date');
+  }
+
+  const timeInMs = to - from;
+  const timeInDays = timeInMs / (1000 * 3600 * 24);
+  
+  console.log(timeInMs, timeInDays, Math.ceil(timeInDays));
+
+  return Math.ceil(timeInDays);
 } 
 
 export const totalPrice = (price, days) => {
