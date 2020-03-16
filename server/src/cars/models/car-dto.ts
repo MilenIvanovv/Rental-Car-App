ab4d317372df8d6714edbd634b93ed32aec9dfbe
Milen Ivanov <ivanovvmilen@gmail.com>
2020-03-16 13:48:38 +0200

    Added car and class DTOs

---
+++ server/src/cars/models/car-dto.ts
@@ -0,0 +1,22 @@
+import { Publish } from '../../transformer/decorators/publish';
+import { CarStatus } from '../../common/car-status.enum';
+import { ClassDTO } from './class-todo';
+
+export class CarDTO {
+  @Publish()
+  id: string;
+
+  @Publish()
+  model: string;
+
+  @Publish()
+  picture: string;
+
+  @Publish()
+  status: CarStatus
+
+  @Publish()
+  class: ClassDTO;
+}
+
+
