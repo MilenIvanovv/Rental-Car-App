6df8e0dcb8e194143d0287d5f2d5568a45a3a964
Milen Ivanov <ivanovvmilen@gmail.com>
2020-03-16 13:49:53 +0200

    Controllers return type dto

--- server/src/cars/cars.controller.ts
+++ server/src/cars/cars.controller.ts
@@ -1,18 +1,19 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { Car } from '../database/entities/cars.entity';
+import { CarDTO } from './models/car-dto';
 
 @Controller('cars')
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
   @Get()
-  async getCars(): Promise<Car[]>  {
+  async getCars(): Promise<CarDTO[]>  {
     return await this.carsService.getCars();
   }
 
   @Get('/:carId')
-  async getCar(@Param('carId') carId: number): Promise<Car>  {
+  async getCar(@Param('carId') carId: number): Promise<CarDTO>  {
     return await this.carsService.getCar(carId);
   }
 }
