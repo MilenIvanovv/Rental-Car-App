a6f392ea6ef80e04cd02826c16a24685c2fbb04b
Milen Ivanov <ivanovvmilen@gmail.com>
2020-03-25 17:33:00 +0200

    Loading indicator on report

--- client/src/components/reportsPage/Report.jsx
+++ client/src/components/reportsPage/Report.jsx
@@ -5,23 +5,27 @@ import './report.css'
 
 export default function Report(props) {
 
-  const { report } = props;
+  const {
+    report,
+    loading
+  } = props;
 
+  const cardText = (
+    <Card.Text className="align-card-text">
+      <span className="section w-50">
+        {report.map((x) => <span key={x.class}>Class: {x.class}</span>)}
+      </span>
+      <span className="section">
+        {report.map((x) => <span key={x.class}>days: {x.averageDays || 0}</span>)}
+      </span>
+    </Card.Text>
+  )
 
   return (
-    <Card className="report-card"
-      
-    >
+    <Card className="report-card">
       <Card.Header>Average days per class</Card.Header>
       <Card.Body>
-        <Card.Text className="align-card-text">
-            <span className="section w-50">
-              {report.map((x) => <span key={x.class}>Class: {x.class}</span>)}
-            </span>
-            <span className="section">
-              {report.map((x) => <span key={x.class}>Class: {x.averageDays || 0}</span>)}
-            </span>
-        </Card.Text>
+        {loading ? <h4>Loading...</h4> : cardText}
       </Card.Body>
     </Card>
   )
