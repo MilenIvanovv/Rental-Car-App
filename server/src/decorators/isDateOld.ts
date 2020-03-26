e8cffc9754a4b3f8012a5a5cbf5f96589929b510
Milen Ivanov <ivanovvmilen@gmail.com>
2020-03-26 14:21:19 +0200

    Date validator small fix

--- server/src/decorators/isDateOld.ts
+++ server/src/decorators/isDateOld.ts
@@ -1,7 +1,7 @@
 import { registerDecorator, ValidationOptions, ValidationArguments } from "class-validator";
 
 export function IsDateOld(property?: string, validationOptions?: ValidationOptions) {
-    return function (object: Object, propertyName: string) {
+    return function (object: any, propertyName: string) {
         registerDecorator({
             name: "IsDateOld",
             target: object.constructor,
