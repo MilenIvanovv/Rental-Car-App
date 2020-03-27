99f3bcc7433afc2110059edbe8a25d354503e5d4
Milen Ivanov <ivanovvmilen@gmail.com>
2020-03-27 12:17:05 +0200

    Average income per class request

--- client/src/components/reportsPage/ReportsPage.jsx
+++ client/src/components/reportsPage/ReportsPage.jsx
@@ -4,6 +4,7 @@ import { Container, Row, Col } from 'react-bootstrap'
 import axios from 'axios';
 import { API_ROOT } from '../../constants/constants';
 import AvgDays from './reportResults/AvgDays';
+import AvgIncomePerMonth from './reportResults/AvgIncomePerMonth';
 import CurrentRentals from './reportResults/CurrentRentals';
 
 
@@ -22,24 +23,33 @@ export default class ReportsPage extends Component {
         data: [],
         loading: false,
       },
+      averageIncomePerClass: {
+        data: [],
+        loading: false,
+      },
     };
   }
 
   render() {
 
-    const { averageDaysPerClass, currentlyRentedCarsPerClass } = this.state;
+    const { averageDaysPerClass, currentlyRentedCarsPerClass, averageIncomePerClass } = this.state;
 
     return (
       <Container>
         <Row className="mb-3">
           <Col xs={4}>
             <Report title="Average days per class" report={averageDaysPerClass}>
-              <AvgDays/>
+              <AvgDays />
             </Report>
           </Col>
           <Col xs={4}>
             <Report title="Current rented cars per class" report={currentlyRentedCarsPerClass}>
-              <CurrentRentals/>
+              <CurrentRentals />
+            </Report>
+          </Col>
+          <Col xs={4}>
+            <Report title="Average income per class per month" report={averageIncomePerClass}>
+              <AvgIncomePerMonth />
             </Report>
           </Col>
         </Row>
@@ -50,24 +60,25 @@ export default class ReportsPage extends Component {
   componentDidMount() {
     this.getReportAverageDaysPerClass();
     this.getReportCurrentlyRentedCarsPerClass();
+    this.getReportAverageIncomePerClass();
   }
 
   async getReportAverageDaysPerClass() {
     let report;
-    this.setState((prevState) => ({ averageDaysPerClass: { ...prevState.averageDaysPerClass, loading: true  } }))
+    this.setState((prevState) => ({ averageDaysPerClass: { ...prevState.averageDaysPerClass, loading: true } }))
     await new Promise((res) => setTimeout(res, 1000));
     try {
       report = await axios.get(`${API_ROOT}/reports/class/averageDays`);
-    this.setState((prevState) => ({ averageDaysPerClass: { ...prevState.averageDaysPerClass, data: report.data  } }))
+      this.setState((prevState) => ({ averageDaysPerClass: { ...prevState.averageDaysPerClass, data: report.data } }))
     } catch (error) {
       console.log(error);
     }
-    this.setState((prevState) => ({ averageDaysPerClass: { ...prevState.averageDaysPerClass, loading: false  } }))
+    this.setState((prevState) => ({ averageDaysPerClass: { ...prevState.averageDaysPerClass, loading: false } }))
   }
 
   async getReportCurrentlyRentedCarsPerClass() {
     let report;
-    this.setState((prevState) => ({ currentlyRentedCarsPerClass: { ...prevState.currentlyRentedCarsPerClass, loading: true  } }))
+    this.setState((prevState) => ({ currentlyRentedCarsPerClass: { ...prevState.currentlyRentedCarsPerClass, loading: true } }))
     await new Promise((res) => setTimeout(res, 1000));
     try {
       report = await axios.get(`${API_ROOT}/reports/class/currentRentedCars`);
@@ -76,5 +87,18 @@ export default class ReportsPage extends Component {
       console.log(error);
     }
     this.setState((prevState) => ({ currentlyRentedCarsPerClass: { ...prevState.currentlyRentedCarsPerClass, loading: false } }))
+  }
+
+  async getReportAverageIncomePerClass() {
+    let report;
+    this.setState((prevState) => ({ averageIncomePerClass: { ...prevState.averageIncomePerClass, loading: true } }))
+    await new Promise((res) => setTimeout(res, 1000));
+    try {
+      report = await axios.get(`${API_ROOT}/reports/class/avgMonthlyIncome?year=2020&month=3`, { year: 2020, month: 3});
+      this.setState((prevState) => ({ averageIncomePerClass: { ...prevState.averageIncomePerClass, data: report.data } }))
+    } catch (error) {
+      console.log(error);
+    }
+    this.setState((prevState) => ({ averageIncomePerClass: { ...prevState.averageIncomePerClass, loading: false } }))
   }
 }
