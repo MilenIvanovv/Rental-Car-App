ce6941b5566eee553d187bfec5f4ba6f4f0df641
Milen Ivanov <ivanovvmilen@gmail.com>
2020-03-31 15:18:38 +0300

    Removed delay in nightwatch test

--- client/src/tests/happy-path.js
+++ client/src/tests/happy-path.js
@@ -31,8 +31,6 @@ module.exports = {
       .setValue('input[data=lastName]', 'Petkov')
       .setValue('input[data=age]', '25')
       .setValue('.react-datepicker-wrapper .react-datepicker__input-container input[type=text]', afterThreeDays)
-      .pause(5000)
-      // .click(`div[aria-label="Choose ${afterThreeDays}"]`)
       .click('button[data=confirm]')
       .waitForElementVisible('[data=current_rentals_model]')
       .assert.containsText('[data=current_rentals_model]', 'Ford')
