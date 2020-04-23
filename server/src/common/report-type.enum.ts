9ed9f00a98e4592ac6f3896820073a59aa1b85d8
Milen Ivanov <ivanovvmilen@gmail.com>
2020-04-23 10:33:18 +0300

    Typo

--- server/src/common/report-type.enum.ts
+++ server/src/common/report-type.enum.ts
@@ -2,5 +2,5 @@ export enum ReportType {
   income = 'income',
   expenses = 'expenses',
   revenue = 'revenue',
-  insuaranceExpense = 'insuaranceExpense',
+  insuaranceExpenses = 'insuaranceExpenses',
 }
