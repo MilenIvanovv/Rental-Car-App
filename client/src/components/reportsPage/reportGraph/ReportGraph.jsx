2041bcf618f3379a387af8c9525a1c88dd6fe8f0
Milen Ivanov <ivanovvmilen@gmail.com>
2020-04-29 14:04:49 +0300

    Fix graph color order

--- client/src/components/reportsPage/reportGraph/ReportGraph.jsx
+++ client/src/components/reportsPage/reportGraph/ReportGraph.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './reportGraph.css';
 import {
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine,
@@ -11,8 +11,8 @@ export default function ReportGraph(props) {
 
   let bars = [];
   const mapReportTypeToColor = ['expense', 'income', 'revenue'];
-  const classColorPalet = [['#d20012', '#0974f0', '#194108'], ['#a08486', '#99c8fb', '#4ec530'], ['#9e6166', '#61a1ec', '#47a526'], ['#9a343d', '#5097ea', '#36811c'], ['#9c1e29', '#2b84ec', '#265c11']];
-  const colors = ['#007bff', '#8884d8', '#d8391f'];
+  const classColorPalet = [['#a08486', '#99c8fb', '#4ec530'], ['#9e6166', '#61a1ec', '#47a526'], ['#9a343d', '#5097ea', '#36811c'], ['#9c1e29', '#2b84ec', '#265c11'], ['#d20012', '#0974f0', '#194108']];
+  const singleRowColorPlate = [['#003f5c'], ['#58508d'], ['#bc5090'], ['#ff6361'], ['#ffa600']];
 
   const getSectionData = (title, columns = [], rows = []) => {
     const sectionData = { name: title };
@@ -27,19 +27,19 @@ export default function ReportGraph(props) {
     if (!bars.length) {
       const keys = Object.keys(sectionData);
       const classes = (keys.length - 1) / rows.length;
-      let count = 0;
+      const colorPlate = rows.length === 1 ? singleRowColorPlate : classColorPalet;
+      let count = -1;
 
       bars = keys
+        .slice(1)
         .map((x, index) => {
-          const c = index % rows.length ? count : count++;
-          // const color = classColorPalet[c % classes][index % rows.length];
-
-          const typeIndex = mapReportTypeToColor.indexOf(mapReportTypeToColor.find((t) => x.includes(t)));
-          const color = classColorPalet[c % classes][typeIndex];
+          const c = index % rows.length ? count : ++count;
+          let typeIndex = mapReportTypeToColor.indexOf(mapReportTypeToColor.find((t) => x.includes(t)));
+          typeIndex = rows.length === 1 ? 0 : typeIndex;
+          const color = colorPlate[c % classes][typeIndex];
 
           return <Bar key={x} dataKey={x} stackId={x.split(' ')[0]} fill={color} />;
-        })
-        .slice(1);
+        });
     }
 
     return sectionData;
